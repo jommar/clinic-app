5b22bdf67f157f8f5537b0ae078d95506d852f49
Jommar Ilagan <jom102386@gmail.com>
2023-08-05 01:23:19 +0800

    Add read endpoints

--- api/controllers/dbController.ts
+++ api/controllers/dbController.ts
@@ -1,11 +1,31 @@
 import { Request, Response } from 'express'
 import models from '../models'
 
-export const create = async (req: Request, res: Response) => {
+export const createDocument = async (req: Request, res: Response) => {
   try {
     const model: string = req.params.model
     const { body } = req
     const document = await models[model].create(body)
+    res.send(document)
+  } catch (e) {
+    res.status(400).json({ message: e.message })
+  }
+}
+
+export const readById = async (req: Request, res: Response) => {
+  try {
+    const { model, id } = req.params
+    const document = await models[model].findById(id)
+    res.send(document)
+  } catch (e) {
+    res.status(400).json({ message: e.message })
+  }
+}
+
+export const readDocument = async (req: Request, res: Response) => {
+  try {
+    const { model } = req.params
+    const document = await models[model].find()
     res.send(document)
   } catch (e) {
     res.status(400).json({ message: e.message })
--- api/routes/db.ts
+++ api/routes/db.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express'
-import { create } from '../controllers/dbController'
+import {
+  createDocument,
+  readById,
+  readDocument,
+} from '../controllers/dbController'
 
 const router = Router()
 
-router.post('/create/:model', create)
+router.post('/create/:model', createDocument)
+router.get('/:model/:id', readById)
+router.get('/:model', readDocument)
 
 export default router
